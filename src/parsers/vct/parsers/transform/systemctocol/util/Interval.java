31e6cb113f09bdbd99cb3f2a5e73d47f3f0c2cd1
AlexZambi <alexandruzambori@gmail.com>
2025-05-28 11:34:19 +0200

    add comments to the interval class

--- src/parsers/vct/parsers/transform/systemctocol/util/Interval.java
+++ src/parsers/vct/parsers/transform/systemctocol/util/Interval.java
@@ -28,35 +28,64 @@ public class Interval {
         this(value, value);
     }
 
+    /**
+     * Create a new interval with different bounds.
+     * 
+     * @param lower the lower bound. if null then it is negative infinity.
+     * @param upper the upper bound. if null then it is positive infinity.
+     */
     public Interval(Bound lower, Bound upper) {
         this.setLower(lower);
         this.setUpper(upper);
     }
 
+    /**
+     * Create a new interval with the same bounds.
+     * 
+     * @param value the interval bound. if null then the resulting interval is [-inf, inf]
+     */
     public Interval(Bound value) {
         this(value, value);
     }
 
+    /**
+     * Build an empty interval.
+     */
     private Interval() {
         this.lower = null;
         this.upper = null;
     }
 
+    /**
+     * Copy constructor.
+     */
     public Interval(Interval other) {
         this.lower = other.lower;
         this.upper = other.upper;
     }
 
+    /**
+     * Empty interval.
+     */
     public static final Interval EMPTY = new Interval();
 
+    /**
+     * Check if the interval is lower bounded.
+     */
     public boolean isLowerBounded() {
         return this.lower.isFinite();
     }
 
+    /**
+     * Check if the interval is upper bounded.
+     */
     public boolean isUpperBounded() {
         return this.upper.isFinite();
     }
 
+    /**
+     * Check if the interval is bounded (i.e. both lower and upper bounded).
+     */
     public boolean isBounded() {
         return this.isLowerBounded() && this.isUpperBounded();
     }
@@ -87,6 +116,13 @@ public class Interval {
         );
     }
 
+    /**
+     * Join two intervals. The result is the smallest interval that contains both.
+     * 
+     * @param a the first interval
+     * @param b the second interval
+     * @return the resulting interval
+     */
     public static Interval join(Interval a, Interval b) {
         return new Interval(
             Bound.min(a.lower, b.lower),
@@ -94,20 +130,48 @@ public class Interval {
         );
     }
 
+    /**
+     * Join an interval to this one. The result is the smallest interval that contains both.
+     * 
+     * @param other the other interval
+     * @return the resulting interval
+     */
     public Interval join(Interval other) {
         return join(this, other);
     }
 
+    /**
+     * Widen two intervals together. If the second interval is wider than the first,
+     * then the result has an infinite bound on that side of the interval.
+     * 
+     * @param a the first interval
+     * @param b the second interval
+     * @return the resulting interval
+     */
     public static Interval widen(Interval a, Interval b) {
         Bound lower = b.lower.compareTo(a.lower) < 0 ? Bound.MINUS_INFINITY : a.lower;
         Bound upper = b.upper.compareTo(a.upper) > 0 ? Bound.INFINITY : a.upper;
         return new Interval(lower, upper);
     }
 
+    /**
+     * Widen two intervals together. If the second interval is wider than the first,
+     * then the result has an infinite bound on that side of the interval.
+     * 
+     * @param other the other interval
+     * @return the resulting interval
+     */
     public Interval widen(Interval other) {
         return widen(this, other);
     }
 
+    /**
+     * Intersect two intervals.
+     * 
+     * @param a the first interval
+     * @param b the second interval
+     * @return the resulting interval
+     */
     public static Interval intersect(Interval a, Interval b) {
         if (a.upper.compareTo(b.lower) < 0 || 
             b.upper.compareTo(a.lower) < 0) {
@@ -116,6 +180,12 @@ public class Interval {
         return new Interval(Bound.max(a.lower, b.lower), Bound.min(a.upper, b.upper));
     }
 
+    /**
+     * Intersect an interval with this one
+     * 
+     * @param other the other interval
+     * @return the resulting interval
+     */
     public Interval intersect(Interval other) {
         return intersect(this, other);
     }
